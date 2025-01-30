2b83f9e6233c8a4b86a9ef20259ac19c6011f564
Hevhs726kshv <felixdsd48@gmail.com>
2025-01-30 17:57:44 -0500

    Got rid of changes to RobotContainer

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -21,7 +21,7 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-  private final Algae_Mechanism algaeMechanism = new Algae_Mechanism();
+  
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController = 
@@ -51,7 +51,7 @@ public class RobotContainer {
     // cancelling on release.
     m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
 
-    algaeMechanism.setDefaultCommand(new )
+    
     
   }
 
