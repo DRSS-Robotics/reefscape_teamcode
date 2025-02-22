24c704cd99311db0f02002bf9b8260022bd1d227
Logan Willinger <logan.willinger@gmail.com>
2025-02-22 10:42:49 -0500

    changed all SwerveRequest.FieldCentric to SwerveRequest.RobotCentric

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -26,7 +26,6 @@ import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 
-// testing that git works on my laptop
 public class RobotContainer {
     private double MaxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
     private double MaxAngularRate = RotationsPerSecond.of(0.35).in(RadiansPerSecond); // 1/4 of a rotation per second max angular velocity
@@ -34,7 +33,7 @@ public class RobotContainer {
     double SlownessModifier = 1;
 
     /* Setting up bindings for necessary control of the swerve drive platform */
-    private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
+    private final SwerveRequest.RobotCentric drive = new SwerveRequest.RobotCentric()
             .withDeadband(MaxSpeed * 0.05 * SlownessModifier).withRotationalDeadband(MaxAngularRate * 0.05 * SlownessModifier) // Add a 10% deadband
             .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive motors
     private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
@@ -50,9 +49,7 @@ public class RobotContainer {
     public final CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
     
     public final SparkMax outtakeMotor = new SparkMax(11, MotorType.kBrushed);
-    // public TalonFXConfigurator = new TalonFXConfigurator();
 
-    /* Path follower */
     private final SendableChooser<Command> autoChooser;
 
     public RobotContainer() {
@@ -71,7 +68,6 @@ public class RobotContainer {
         // Note that X is defined as forward according to WPILib convention,
         // and Y is defined as to the left according to WPILib convention.
         drivetrain.setDefaultCommand(
-            // Drivetrain will execute this command periodically
             drivetrain.applyRequest(() ->
                 drive.withVelocityX(-joystick.getLeftY() * MaxSpeed * speedScalar * SlownessModifier) // Drive forward with negative Y (forward)
                     .withVelocityY(-joystick.getLeftX() * MaxSpeed * speedScalar * SlownessModifier) // Drive left with negative X (left)
@@ -79,8 +75,6 @@ public class RobotContainer {
             )
         );
 
-        // set these to joystick2 later
-        // We fixed it for you- Micah and William L.
         joystick2.a().whileTrue(Commands.run(() -> {
             outtakeMotor.set(0.75);
         }));
@@ -99,12 +93,7 @@ public class RobotContainer {
                 .withRotationalRate(-0.25 * Math.PI) // clockwise
             )
         );
-        // joystick.x().whileTrue(drivetrain.applyRequest(() ->
-        //     drive.withVelocityX(-0.5) // forward
-        //         .withVelocityY(0)
-        //         .withRotationalRate(0)
-        //     // point.withAngle(Rotation2d.fromDegrees(0));
-        //     )
+
         joystick.rightBumper().whileTrue(Commands.run(() -> SlownessModifier = 0.35));
         joystick.rightBumper().whileFalse(Commands.run(() -> SlownessModifier = 1));
 
@@ -117,22 +106,15 @@ public class RobotContainer {
             forwardStraight.withVelocityX(-0.5).withVelocityY(0))
         );
 
-        // Run SysId routines when holding back/start and X/Y.
-        // Note that each routine should be run exactly once in a single log.
         joystick.back().and(joystick.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
         joystick.back().and(joystick.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
         joystick.start().and(joystick.y()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
         joystick.start().and(joystick.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
-        // joystick.leftBumper().onTrue(Commands.runOnce(SignalLogger::start));
-        // joystick.rightBumper().onTrue(Commands.runOnce(SignalLogger::stop));
-
-        // reset the field-centric heading on left bumper press
-        joystick.leftBumper().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldCentric()));
+        
         drivetrain.registerTelemetry(logger::telemeterize);
     }
 
     public Command getAutonomousCommand() {
-        /* Run the path selected from the auto chooser */
         return autoChooser.getSelected();
     }
 }
