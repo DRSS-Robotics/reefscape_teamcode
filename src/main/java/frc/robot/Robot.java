09f2ff7b88cdc6176ab3e2a6cf23bb2f22852e6e
Micah-Lander <lander.22@daytonstemschool.org>
2025-02-24 18:56:44 -0500

    Started working with networktables to get the range and yaw to the telop to align to score higher up coral.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,10 +5,13 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 public class Robot extends TimedRobot {
+    XboxController controller = new XboxController(0);
+    private CameraSubsystem cam = new CameraSubsystem();
   private Command m_autonomousCommand;
 
   private final RobotContainer m_robotContainer;
@@ -19,7 +22,8 @@ public class Robot extends TimedRobot {
 
   @Override
   public void robotPeriodic() {
-    CommandScheduler.getInstance().run(); 
+    CommandScheduler.getInstance().run();
+    cam.readCamera();
   }
 
   @Override
