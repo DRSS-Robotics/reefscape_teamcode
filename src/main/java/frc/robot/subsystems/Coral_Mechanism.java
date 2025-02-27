23c5f0d6a66d2ab9062677dc6323da8cbea94cc7
Hevhs726kshv <94800190+Hevhs726kshv@users.noreply.github.com>
2025-02-27 18:44:03 -0500

    Update Coral_Mechanism.java
    i added a space

--- src/main/java/frc/robot/subsystems/Coral_Mechanism.java
+++ src/main/java/frc/robot/subsystems/Coral_Mechanism.java
@@ -8,7 +8,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotContainer;
 
 
-public class Coral_Mechanism extends SubsystemBase{
+public class Coral_Mechanism extends SubsystemBase {
 
     public boolean coralActivate;
 
