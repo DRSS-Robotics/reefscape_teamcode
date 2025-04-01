447608784b814a623418ab596b59767ad69dbdc0
Lewisv2 <Lewisv.2@dayonstemschool.org>
2025-04-01 17:51:10 -0400

    added some (untested) functionality for automatic path generation using april tag id's

---
+++ src/main/java/frc/commands/AutoPathfindToAprilTagCommand.java
@@ -0,0 +1,71 @@
+package frc.commands;
+
+import java.util.List;
+
+import com.pathplanner.lib.path.GoalEndState;
+import com.pathplanner.lib.path.PathConstraints;
+import com.pathplanner.lib.path.PathPlannerPath;
+import com.pathplanner.lib.path.Waypoint;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.CameraSubsystem;
+
+public class AutoPathfindToAprilTagCommand extends Command {
+
+    CameraSubsystem Cam;
+    Pose2d TargetPos;
+    Rotation2d DesiredRotation;
+    // false = left of reef, true = right of reef
+    boolean Direction;
+
+    public AutoPathfindToAprilTagCommand(CameraSubsystem Camera, boolean ReefDirection) {
+        Cam = Camera;
+        Direction = ReefDirection;
+    }
+
+    @Override
+    public void initialize() {
+        System.out.println(Cam.closestID);
+
+        switch (Cam.closestID) {
+            case 7:
+                DesiredRotation = new Rotation2d(180.0);
+                TargetPos = Direction ? new Pose2d(14.381, 3.781, DesiredRotation)
+                        : new Pose2d(14.381, 4.09, DesiredRotation);
+                break;
+
+            case 9:
+                DesiredRotation = new Rotation2d(-60.0);
+                TargetPos = Direction ? new Pose2d(12.636, 5.302, DesiredRotation)
+                        : new Pose2d(12.363, 5.136, DesiredRotation);
+                break;
+
+            case 11:
+                DesiredRotation = new Rotation2d(60.0);
+                TargetPos = Direction ? new Pose2d(12.168, 3.018, DesiredRotation)
+                        : new Pose2d(12.443, 2.862, DesiredRotation);
+                break;
+
+            default:
+                TargetPos = new Pose2d(0.0, 0.0, Rotation2d.fromDegrees(0));
+                break;
+        }
+
+        List<Waypoint> waypoints = PathPlannerPath.waypointsFromPoses(
+                new Pose2d(1.0, 1.0, Rotation2d.fromDegrees(0)),
+                TargetPos);
+
+        PathConstraints constraints = new PathConstraints(3.0, 3.0, 2 * Math.PI, 4 * Math.PI);
+
+        PathPlannerPath path = new PathPlannerPath(
+                waypoints,
+                constraints,
+                null,
+                new GoalEndState(0.0, DesiredRotation));
+
+        path.preventFlipping = true;
+    }
+
+}
--- src/main/java/frc/robot/CameraSubsystem.java
+++ src/main/java/frc/robot/CameraSubsystem.java
@@ -1,4 +1,5 @@
 package frc.robot;
+
 import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonUtils;
 import org.photonvision.targeting.PhotonPipelineResult;
@@ -14,106 +15,76 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.networktables.NetworkTableValue;
 import edu.wpi.first.wpilibj.XboxController;
 
-
-public class CameraSubsystem {  
-      public PhotonCamera camera;
-      private XboxController controller;
-      
-          public CameraSubsystem() {
-                camera = new PhotonCamera("April_RGB_Cam");
-              controller = new XboxController(0);
-                System.out.println("works");
-        /*  return camera/controller */
-      }
+public class CameraSubsystem {
+  public PhotonCamera camera;
+  public PhotonTrackedTarget closestTarget;
+  public double closestYaw = 0.0;
+  public Transform3d closestOffset;
+  public int closestID;
 
 
-      public void readCamera() {
-       // AprilTagFieldLayout tagFieldLayout = AprilTagFieldLayout.loadField(AprilTagFields.k2025Reefscape);
-        boolean targetVisible = false;
-        double targetYaw = 0.0;
-        Transform3d targetOffset;
-        double targetRange = 0.0;
+  public CameraSubsystem() {
+    camera = new PhotonCamera("April_RGB_Cam");
+    System.out.println("works");
+    /* return camera/controller */
+  }
 
-        double closestRange = -1.0;
-        double cameraPitch = 0.0;
-        PhotonTrackedTarget closestTarget;
-        double closestYaw = 0.0;
-        Transform3d closestOffset;
-        int closestID;
+  public void readCamera() {
+    // AprilTagFieldLayout tagFieldLayout =
+    // AprilTagFieldLayout.loadField(AprilTagFields.k2025Reefscape);
+    boolean targetVisible = false;
+    double targetYaw = 0.0;
+    Transform3d targetOffset;
+    double targetRange = 0.0;
 
-        var results = camera.getAllUnreadResults();
-        if (!results.isEmpty()) {
-            PhotonPipelineResult result = results.get(results.size()-1);
-            if (result.hasTargets()){
-                for (var target : result.getTargets()){
-                  //   if (target.getFiducialId() == 7)
-                    int targetID = target.getFiducialId();
-                    // if () {
-                        /* Found Tag, record its information */
-                        targetYaw = target.getYaw();
-                        targetOffset = target.getBestCameraToTarget();                      
-                        targetRange =
-                        PhotonUtils.calculateDistanceToTargetMeters(
-                            0.025, /*The height of the camera in meters */
-                            0.22225, /*height of apriltag from ground to bottom */
-                                Units.degreesToRadians(cameraPitch), 
-                                Units.degreesToRadians(target.getPitch()));
-        
-                        targetVisible = true;
-                        // checks if the target is the current closest target and then saves its values
-                        if (targetRange < closestRange || closestRange == -1){
-                          closestRange = targetRange;
-                          closestTarget = target;
-                          closestYaw = targetYaw;
-                          closestOffset = targetOffset;
-                          // the targetID is saved, since it may be useful to display to the driver
-                          // in the future
-                          closestID = targetID;
-                        }
-                        if (controller.getAButton() && targetVisible) {
-                            System.out.println("ID:");
-                            System.out.println(targetID); /*print the ID */
-                            System.out.println("Range:");
-                            System.out.println(targetRange); /*print the range */
-                            System.out.println("Yaw:");
-                            System.out.println(targetYaw); /*print the yaw */
-                            System.out.println("Offset:");
-                            System.out.println(targetOffset); /*print the yaw */
-                          }
-                    }
-                    // print for testing the closest target values
-                    System.out.println("Closest Range:");
-                    System.out.println(closestRange);
-                    NetworkTableInstance inst = NetworkTableInstance.getDefault();
-                    NetworkTable table = inst.getTable("CameraData");
-                                   
-          
-                }
+    double closestRange = -1.0;
+    double cameraPitch = 0.0;
+    var results = camera.getAllUnreadResults();
+    if (!results.isEmpty()) {
+      PhotonPipelineResult result = results.get(results.size() - 1);
+      if (result.hasTargets()) {
+        for (var target : result.getTargets()) {
+          // if (target.getFiducialId() == 7)
+          int targetID = target.getFiducialId();
+          /* Found Tag, record its information */
+          targetYaw = target.getYaw();
+          targetOffset = target.getBestCameraToTarget();
+          targetRange = PhotonUtils.calculateDistanceToTargetMeters(
+              0.025, /* The height of the camera in meters */
+              0.22225, /* height of apriltag from ground to bottom */
+              Units.degreesToRadians(cameraPitch),
+              Units.degreesToRadians(target.getPitch()));
+
+          targetVisible = true;
+          // checks if the target is the current closest target and then saves its values
+          if (targetRange < closestRange || closestRange == -1) {
+            closestRange = targetRange;
+            closestTarget = target;
+            closestYaw = targetYaw;
+            closestOffset = targetOffset;
+            // the targetID is saved, since it may be useful to display to the driver
+            // in the future
+            closestID = targetID;
+          }
+          if (targetVisible) {
+            System.out.println("ID:");
+            System.out.println(targetID); /* print the ID */
+            System.out.println("Range:");
+            System.out.println(targetRange); /* print the range */
+            System.out.println("Yaw:");
+            System.out.println(targetYaw); /* print the yaw */
+            System.out.println("Offset:");
+            System.out.println(targetOffset); /* print the yaw */
+          }
         }
-      //  }
-        // if (controller.getAButton() && targetVisible) {
-        //     // System.out.println("ID:");
-        //     // System.out.println(targetID);
-        //     System.out.println("Range:");
-        //     System.out.println(targetRange);
-        //     System.out.println("Yaw:");
-        //     System.out.println(targetYaw);
-        //   }
+        // print for testing the closest target values
+        System.out.println("Closest Range:");
+        System.out.println(closestRange);
+        NetworkTableInstance inst = NetworkTableInstance.getDefault();
+        NetworkTable table = inst.getTable("CameraData");
 
-            if(controller.getBButton() && targetVisible) {
-              System.out.println("dijzgh.gkcvjoz.xkchfuv");
-              System.out.println(targetRange - targetYaw);
-              if(targetRange == 0) 
-              if(targetRange == 0) {
-                System.out.println("You are there");
-              }
-              else if(targetRange > 0 && targetYaw > 0) {
-
-              }
-              
-            }
-       
-            
       }
-      
     }
+
+  }
+}
